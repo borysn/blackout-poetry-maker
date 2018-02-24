8d23e26e815fc600bf9114a55709d7412310d704
tsuki KJHANONICHI <ktsukik@hotmail.com>
2018-02-24 16:34:54 -0600

    choose your words...wisely

--- src/components/word.jsx
+++ src/components/word.jsx
@@ -3,12 +3,20 @@ import React, {Component} from 'react';
 class Word extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selected: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({selected: (this.state.selected ? false : true)});
   }
 
   render() {
     return (
       <div className='word-container'>
-        <div className='word'>
+        <div className={this.state.selected ? 'word-selected' : 'word'} onClick={this.handleClick}>
           <span className='word-value'>{this.props.value}</span>
         </div>
       </div>
