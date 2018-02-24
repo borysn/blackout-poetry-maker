4932951926491e0e2c362557dc59dac7ab3ba174
borysn <ktsukik@hotmail.com>
2018-02-24 02:40:02 -0600

    remove prop, add settimoeout

--- src/components/inputtext.jsx
+++ src/components/inputtext.jsx
@@ -3,13 +3,11 @@ import React, {Component} from 'react';
 class InputText extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      poem: this.props.poem
-    };
     this.handleTextChange = this.handleTextChange.bind(this);
   }
 
   handleTextChange(e) {
+    setTimeout(1000);
     this.props.onTextChange(e.target.value);
   }
 
@@ -20,7 +18,9 @@ class InputText extends Component {
          <span>1: input text</span>
        </div>
        <div id='inputtext'>
-         <textarea id='inputtextarea' rows='13' cols='43' onChange={this.handleTextChange}></textarea>
+         <textarea id='inputtextarea' rows='13' cols='43' 
+           onChange={this.handleTextChange}>
+         </textarea>
        </div>
       </div>
     );
