11451830b2768194b0869771e491ede092fb73c2
tsuki KJHANONICHI <ktsukik@hotmail.com>
2018-02-24 18:27:45 -0600

    format over function

---
+++ src/components/formatitem.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class FormatItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: props.selected
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.toggle(e, this.props.index);
+  }
+
+  render() {
+    return (
+      <div className={(this.props.selected ? 'format-item-selected' : 'format-item')}
+        onClick={this.handleClick}>
+        <span className='format-item-value'>{this.props.value}</span>
+      </div>
+    );
+  }
+}
+
+export default FormatItem;
--- src/components/formattext.jsx
+++ src/components/formattext.jsx
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
+import FormatItem from './formatitem.jsx';
 
 class FormatText extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formats: ['░', '▒', '▓', '█'],
+      selected: 3
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(e, index) {
+    this.setState({selected: index});
+  }
+
   render() {
+    const items = this.state.formats.map((v, i) => {
+      return (
+        <FormatItem
+          selected={(this.state.selected === i ? true : false)}
+          key={i}
+          index={i}
+          value={v}
+          toggle={this.toggle}
+        />
+      );
+    });
+
     return (
       <div id='formattext-container' className='section-container'>
         <div className='section-title'>
           <span>3: format text</span>
         </div>
         <div id='formattext'>
-          format text
+          <div id='format-item-container'>
+            {items}
+          </div>
         </div>
       </div>
     );
